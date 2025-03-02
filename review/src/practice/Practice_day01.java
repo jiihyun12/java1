5d776a58212325582f6cd1c5b71613f459c4aeb5
jiihyun <j22h.h0h@gmail.com>
2025-03-02 11:05:54 +0900

    250302 복습

---
+++ review/src/practice/Practice_day01.java
@@ -0,0 +1,848 @@
+package practice;
+
+public class Practice_day01 {
+	public static void main(String[] args) {
+		
+//  	variable
+//		초기값
+//		1.
+		int = 0;
+		long = 0L;
+		float = 0.0F;
+		char = 공백문자;
+		double = 0.0;
+		boolean = false;
+		String = "", NULL
+		
+//		2.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		3.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		4.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		5.
+		int 0
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		6.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		7.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		8.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "",NULL
+		
+//		9.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		10.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		11.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		12.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		13.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		14.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "", NULL
+		
+//		15.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "",NULL
+		
+//		16.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		boolean false
+		String "",NULL
+		
+//		17.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		Sting ::, NULL
+		
+//		18.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		Stirng "",NULL
+		
+//		19.
+		int 0
+		long 0L
+		float 0.0
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		20.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		21.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		22.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		23.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		24.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		25.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		26.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		27.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		28.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		29.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		30.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		31. 
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		32.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		33.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		34.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		35.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		36.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "",NULL
+		
+//		37.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		38.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		39.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		40.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		41.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		42.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		Stirng "", NULL
+		
+//		43.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		44.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자 
+		double 0.0
+		String "", NULL
+		
+//		45.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		46.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		47.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		47.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		48.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		49.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		50.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		51.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		52.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		53.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		54.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		55.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		56.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		57.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		58.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		59.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		60.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		61.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		62.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		63.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		64.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		65.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		66.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		67.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		68.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		69.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		70.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		71.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		72.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		73.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		74.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		75.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		76.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		77.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+		
+//		78.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+		
+//		79.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+		
+//		80.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		81.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+		
+//		82.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		83.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		84.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		85.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		86.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		87.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		88.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		89.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		90.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+		
+//		91.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+		
+//		92.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+		
+//		93.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		94.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		95.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		96.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		97.
+		int 0
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		98.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		99.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+//		100.
+		int 0 
+		long 0L
+		float 0.0F
+		char 공백문자
+		double 0.0
+		String "", NULL
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+	}	
+}
